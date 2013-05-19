801ed442f2f58dbb818d47d0cd23a0445f5b3e64
Darcy Laycock <sutto@sutto.net>
2013-05-19 18:05:47 +0800

    Add in remove.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Filter Squad"
 maintainer_email "darcy@filtersquad.com"
 license          "Apache 2.0"
-description      "Installs pyapns on a given machine"
+description      "Installs / removes pyapns on a given machine"
 version          "1.0"
 
 depends "python"
---
+++ recipes/remove.rb
@@ -0,0 +1,44 @@
+include_recipe 'python'
+
+config       = node['pyapns']
+service_name = config['service_name']
+
+group_name = config['group']
+user_name  = config['user']
+home_dir   = config['home']
+
+
+service service_name do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action   [:stop, :disable]
+end
+
+file "/etc/init/#{service_name}.conf" do
+  action :delete
+end
+
+python_pip "pyapns" do
+  action :remove
+end
+
+group group_name do
+  system true
+  action :remove
+end
+
+user user_name do
+  gid     group_name
+  home    home_dir
+  comment "PyAPNS user"
+  shell   "/bin/bash"
+  system  true
+  action :remove
+end
+
+directory home_dir do
+  owner     user_name
+  group     group_name
+  recursive true
+  action :delete
+end
