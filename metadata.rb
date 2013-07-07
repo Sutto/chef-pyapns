29cddeecf19b03f1b5f9c8ad954cbe5149fc723d
Darcy Laycock <sutto@sutto.net>
2013-07-07 20:35:42 +0800

    Bump the version.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Filter Squad"
 maintainer_email "darcy@filtersquad.com"
 license          "Apache 2.0"
 description      "Installs / removes pyapns on a given machine"
-version          "1.0"
+version          "2.0"
 
 depends "python"
 
